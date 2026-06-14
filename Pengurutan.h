0f55ec3b1e666070aa7ee5994d74505b6a8dc505
2503010102-bit <2503010102@unper.ac.id>
2026-06-14 12:22:47 +0700

    Pengurutan.h

--- Pengurutan.h
+++ Pengurutan.h
@@ -1,1 +1,17 @@
+#ifndef PENGURUTAN_H
+#define PENGURUTAN_H
 
+#include <iostream>
+#include <string>
+
+#include "siswa.h"
+#include "nilai.h"
+
+using namespace std;
+
+// Sorting Data
+void urutkanSiswaByNIS();
+void urutkanSiswaByNama();
+void urutkanNilaiTertinggi();
+
+#endif
