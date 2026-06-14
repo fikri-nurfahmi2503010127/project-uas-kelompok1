a0b637b2173bf583868b5077adf939ebaddb467f
Hazmy Hizbul Izza <2503010108@unper.ac.id>
2026-06-14 10:01:14 +0700

    Add Mapel structure and CRUD function declarations

--- mapel.h
+++ mapel.h
@@ -1,1 +1,22 @@
+#ifndef MAPEL_H
+#define MAPEL_H
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Mapel {
+    string kodeMapel;
+    string namaMapel;
+    string guruPengampu;
+    Mapel* next;
+};
+
+extern Mapel* headMapel;
+
+// CRUD Mata Pelajaran
+void tambahMapel();
+void tampilMapel();
+
+#endif
