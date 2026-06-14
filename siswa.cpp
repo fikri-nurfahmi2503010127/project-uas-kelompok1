60cc0a6524f97861d26ee163ec87027422589901
2503010134-MuhammadGustavAbdillah <2503010134@unper.ac.id>
2026-06-14 13:28:59 +0700

    Update siswa.cpp

--- siswa.cpp
+++ siswa.cpp
@@ -1,1 +1,63 @@
+#include "siswa.h"
 
+Siswa* headSiswa = NULL;
+
+// Tambah Data Siswa
+void tambahSiswa() {
+    Siswa* baru = new Siswa();
+
+    cout << "\n=== Tambah Data Siswa ===\n";
+    cout << "NIS    : ";
+    cin >> baru->nis;
+    cin.ignore();
+
+    cout << "Nama   : ";
+    getline(cin, baru->nama);
+
+    cout << "Kelas  : ";
+    getline(cin, baru->kelas);
+
+    cout << "Alamat : ";
+    getline(cin, baru->alamat);
+
+    baru->next = NULL;
+
+    if (headSiswa == NULL) {
+        headSiswa = baru;
+    } else {
+        Siswa* temp = headSiswa;
+
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+
+        temp->next = baru;
+    }
+
+    cout << "\nData siswa berhasil ditambahkan!\n";
+}
+
+// Tampilkan Data Siswa
+void tampilSiswa() {
+
+    if (headSiswa == NULL) {
+        cout << "\nData siswa masih kosong.\n";
+        return;
+    }
+
+    cout << "\n=== DAFTAR SISWA ===\n";
+
+    Siswa* temp = headSiswa;
+    int no = 1;
+
+    while (temp != NULL) {
+
+        cout << "\nData Ke-" << no++ << endl;
+        cout << "NIS    : " << temp->nis << endl;
+        cout << "Nama   : " << temp->nama << endl;
+        cout << "Kelas  : " << temp->kelas << endl;
+        cout << "Alamat : " << temp->alamat << endl;
+
+        temp = temp->next;
+    }
+}
