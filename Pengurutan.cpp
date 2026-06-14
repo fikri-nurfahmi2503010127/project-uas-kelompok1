57a026cc74c602aab1f55430e9a5a32110a7db95
2503010102-bit <2503010102@unper.ac.id>
2026-06-14 12:28:10 +0700

    Pengurutan.cpp

--- Pengurutan.cpp
+++ Pengurutan.cpp
@@ -1,1 +1,37 @@
 
+#include "pengurutan.h"
+
+// Sorting Siswa berdasarkan NIS (Ascending)
+void urutkanSiswaByNIS() {
+
+    if (headSiswa == NULL || headSiswa->next == NULL) {
+        cout << "\nData siswa tidak cukup untuk diurutkan.\n";
+        return;
+    }
+
+    bool tukar;
+
+    do {
+        tukar = false;
+
+        Siswa* current = headSiswa;
+
+        while (current->next != NULL) {
+
+            if (current->nis > current->next->nis) {
+
+                swap(current->nis, current->next->nis);
+                swap(current->nama, current->next->nama);
+                swap(current->kelas, current->next->kelas);
+                swap(current->alamat, current->next->alamat);
+
+                tukar = true;
+            }
+
+            current = current->next;
+        }
+
+    } while (tukar);
+
+    cout << "\nData siswa berhasil diurutkan berdasarkan NIS.\n";
+}
