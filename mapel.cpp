4a85248bf8096c0a97737295cd9ccb9ac97e1428
Hazmy Hizbul Izza <2503010108@unper.ac.id>
2026-06-14 10:03:46 +0700

    Implement functions to add and display subjects

--- mapel.cpp
+++ mapel.cpp
@@ -1,1 +1,63 @@
+#include "mapel.h"
+
+Mapel* headMapel = NULL;
+
+// Tambah Mata Pelajaran
+void tambahMapel() {
+
+    Mapel* baru = new Mapel();
+
+    cout << "\n=== Tambah Mata Pelajaran ===\n";
+
+    cout << "Kode Mata Pelajaran : ";
+    cin >> baru->kodeMapel;
+    cin.ignore();
+
+    cout << "Nama Mata Pelajaran : ";
+    getline(cin, baru->namaMapel);
+
+    cout << "Guru Pengampu       : ";
+    getline(cin, baru->guruPengampu);
+
+    baru->next = NULL;
+
+    if (headMapel == NULL) {
+        headMapel = baru;
+    } else {
+
+        Mapel* temp = headMapel;
+
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+
+        temp->next = baru;
+    }
+
+    cout << "\nData mata pelajaran berhasil ditambahkan.\n";
+}
+
+// Tampilkan Mata Pelajaran
+void tampilMapel() {
+
+    if (headMapel == NULL) {
+        cout << "\nData mata pelajaran masih kosong.\n";
+        return;
+    }
+
+    cout << "\n=== DAFTAR MATA PELAJARAN ===\n";
+
+    Mapel* temp = headMapel;
+    int no = 1;
+
+    while (temp != NULL) {
+
+        cout << "\nData Ke-" << no++ << endl;
+        cout << "Kode Mapel    : " << temp->kodeMapel << endl;
+        cout << "Nama Mapel    : " << temp->namaMapel << endl;
+        cout << "Guru Pengampu : " << temp->guruPengampu << endl;
+
+        temp = temp->next;
+    }
+}
 
