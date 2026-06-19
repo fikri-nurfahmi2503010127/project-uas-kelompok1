333cf40526bc157d5cfd8854caf5fbc481586a61
2503010102-bit <2503010102@unper.ac.id>
2026-06-19 08:06:11 +0700

    Pengurutan.cpp

--- Pengurutan.cpp
+++ Pengurutan.cpp
@@ -1,5 +1,6 @@
 
 #include "pengurutan.h"
+#include <algorithm>
 
 // Sorting Siswa berdasarkan NIS (Ascending)
 void urutkanSiswaByNIS() {
@@ -34,4 +35,78 @@ void urutkanSiswaByNIS() {
     } while (tukar);
 
     cout << "\nData siswa berhasil diurutkan berdasarkan NIS.\n";
+}
+
+// Sorting Siswa berdasarkan Nama (Ascending)
+void urutkanSiswaByNama() {
+
+    if (headSiswa == NULL || headSiswa->next == NULL) {
+        cout << "\nData siswa tidak cukup untuk diurutkan.\n";
+        return;
+    }
+
+    bool tukar;
+
+    do {
+        tukar = false;
+
+        Siswa* current = headSiswa;
+
+        while (current->next != NULL) {
+
+            if (current->nama > current->next->nama) {
+
+                swap(current->nis, current->next->nis);
+                swap(current->nama, current->next->nama);
+                swap(current->kelas, current->next->kelas);
+                swap(current->alamat, current->next->alamat);
+
+                tukar = true;
+            }
+
+            current = current->next;
+        }
+
+    } while (tukar);
+
+    cout << "\nData siswa berhasil diurutkan berdasarkan Nama.\n";
+}
+
+// Sorting Nilai Tertinggi ke Terendah
+void urutkanNilaiTertinggi() {
+
+    if (topNilai == NULL || topNilai->next == NULL) {
+        cout << "\nData nilai tidak cukup untuk diurutkan.\n";
+        return;
+    }
+
+    bool tukar;
+
+    do {
+        tukar = false;
+
+        Nilai* current = topNilai;
+
+        while (current->next != NULL) {
+
+            if (current->nilai < current->next->nilai) {
+
+                swap(current->nis,
+                     current->next->nis);
+
+                swap(current->mataPelajaran,
+                     current->next->mataPelajaran);
+
+                swap(current->nilai,
+                     current->next->nilai);
+
+                tukar = true;
+            }
+
+            current = current->next;
+        }
+
+    } while (tukar);
+
+    cout << "\nData nilai berhasil diurutkan dari tertinggi ke terendah.\n";
 }
