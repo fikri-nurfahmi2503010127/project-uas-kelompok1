a79cd4e242c0bc780329a07e2da578101e5dfea1
2503010118-collab <2503010118@unper.ac.id>
2026-06-24 14:29:54 +0700

    pencarian.h

---
+++ pencarian.h
@@ -0,0 +1,16 @@
+#ifndef PENCARIAN_H
+#define PENCARIAN_H
+
+#include <iostream>
+#include <string>
+
+#include "siswa.h"
+#include "mapel.h"
+
+using namespace std;
+
+// Pencarian Data
+void cariSiswaByNIS();
+void cariMapelByKode();
+
+#endif
